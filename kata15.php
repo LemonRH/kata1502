c724b5cd5a1ad2005778aac046f58cc4d290bb78
LemonRH <82542162+LemonRH@users.noreply.github.com>
2024-02-15 11:18:45 +0100

    kata version2
    Hago otra funcion que no hace uso de in_array porque me sobró tiempo

--- kata15.php
+++ kata15.php
@@ -17,6 +17,25 @@ function buscadorNumeros(){
 
 }
 
-buscadorNumeros();
+function buscadorNumerosManual(){
+    $numInicial2 = readline("Introduce el número del cual quieres comprobar si tiene una cifra: ");
+    $numComprobar2 = readline("Ahora introduce la cifra que quieres comprobar:");
+    $numArray2 = array_map('intval', str_split($numInicial2));
+    $dentroNumero = False;
+
+    for ($i = 0; $i<(count($numArray2)); $i++){
+        if ($numArray2[$i]==$numComprobar2){
+            $dentroNumero=True;
+        }
+
+    }
+    if($dentroNumero=TRUE){
+    echo "La cifra ", $numComprobar2," existe en el numero ", $numInicial2;
+    }else{
+        echo "La cifra ", $numComprobar2," NO existe en el numero ", $numInicial2;
+    }
+}
+
+buscadorNumerosManual();
 
 ?>
